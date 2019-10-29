542c24958ff2eaae6e0b5e7967c3621b3e942b9a
pnavarre82 <pedroandresblaco@gmail.com>
2019-10-29 20:27:30 +0100

    Implements timeParser

--- BerlinClock/Classes/TimeParser.cs
+++ BerlinClock/Classes/TimeParser.cs
@@ -2,11 +2,23 @@
 
 namespace BerlinClock.Classes
 {
-    public class TimeParser
+    public class TimeParser: ITimeParser
     {
         public TimeSpan parseTime(string stringTime)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var spliteTime = stringTime.Split(':');
+                var hours = int.Parse(spliteTime[0]);
+                var minutes = int.Parse(spliteTime[1]);
+                var seconds = int.Parse(spliteTime[2]);
+                return new TimeSpan(hours, minutes, seconds);
+
+            }
+            catch
+            {
+                throw new Exception($"Parse time error time not valid '{stringTime}'");
+            }
         }
     }
 }
